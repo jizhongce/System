474daaeb9141368e4a14a5c0bf4777ecead40ef5
jizhongce <jizhongce123@gmail.com>
2018-01-26 16:35:57 -0500

    Finish SEND SMS function, test for many times and preparing to make it become function and import to sign up section

--- Web Data Sys/Server/SMS_utli.py
+++ Web Data Sys/Server/SMS_utli.py
@@ -7,128 +7,139 @@ import hmac
 import hashlib
 import base64
 import requests
-'''
+#
+#
+# '''
+#
+# #This is the accesskey and accessSecret
+# accessKeyId = "testId"
+#
+# accessSecret = "testSecret";
+#
+# #System parameter
+#
+# #This is the SignatureMethod
+# SignatureMethod = {"SignatureMethod" : "HMAC-SHA1"}
+#
+# #This is the SignatureNonce
+# SignatureNonce = {"SignatureNonce" : str(uuid.uuid4())}
+#
+# #This is the AccessKeyId
+# AccessKeyId = {"AccessKeyId" : accessKeyId}
+#
+# #This is the SignatureVersion
+# SignatureVersion = {"SignatureVersion" : "1.0"}
+#
+# #This is the Timestamp with type: yyyy-MM-dd'T'HH:mm:ss'Z'
+# Timestamp = {"Timestamp" : datetime.datetime.now(tz = timezone('GMT')).strftime("%Y-%m-%dT%H:%M:%SZ")}
+#
+# #This is the Format
+# Format = {"Format" : "XML"}
+#
+# #API parameter
+#
+# #This is Action, "SendSms"
+# Action = {"Action" : "SendSms"}
+#
+# #This is Version
+# Version = {"Version" : "2017-05-25"}
+#
+# #This is RegionId
+# RegionId = {"RegionId" : "cn-hangzhou"}
+#
+# #This is PhoneNumbers
+# PhoneNumbers = {"PhoneNumbers" : "15300000001"}
+#
+# #This sign name SignName
+# SignName = {"SignName" :  "季中策"}
+#
+# #This is template param
+# TemplateParam = {"TemplateParam" : "{\"Code\":\"1234\"}"}
+#
+# #This is TemplateCode
+# TemplateCode = {"TemplateCode" : "SMS_122299912"}
+#
+# #This is OutId
+# OutId = {"OutId" : "123"}
+#
+# '''
+#
 
-#This is the accesskey and accessSecret
-accessKeyId = "testId"
+def SendSMS(PhoneNum):
+    # This is the accesskey and accessSecret
+    accessKeyId = "LTAIWAFaI2a8kv8n"
 
-accessSecret = "testSecret";
+    accessSecret = "PX0FiQFOTauO0rt7WAtZR4l7s7FILw"
 
-#System parameter
+    QueryDict = {
+                "SignatureMethod" : "HMAC-SHA1",
+                "SignatureNonce" : str(uuid.uuid4()),
+                "AccessKeyId" : accessKeyId,
+                "SignatureVersion" : "1.0",
+                "Timestamp" : datetime.datetime.now(tz = timezone('GMT')).strftime("%Y-%m-%dT%H:%M:%SZ"),
+                "Format" : "XML",
+                "Action" : "SendSms",
+                "Version" : "2017-05-25",
+                "RegionId" : "cn-hangzhou",
+                "PhoneNumbers" : PhoneNum,
+                "SignName" :  "季中策",
+                "TemplateParam" : "{\"Code\":\"1234\"}",
+                "TemplateCode" : "SMS_122299912",
+                "OutId" : "123"
+                }
 
-#This is the SignatureMethod
-SignatureMethod = {"SignatureMethod" : "HMAC-SHA1"}
+    parameterStr = Construct_Parameter(QueryDict)
 
-#This is the SignatureNonce
-SignatureNonce = {"SignatureNonce" : str(uuid.uuid4())}
+    #Then add POP with HTTPMethod + “&” + specialUrlEncode(“/”) + ”&” + specialUrlEncode(sortedQueryString)
+    stringToSign = urllib.parse.quote_plus(parameterStr).replace("%7E", "~")
 
-#This is the AccessKeyId
-AccessKeyId = {"AccessKeyId" : accessKeyId}
+    stringToSign = "GET" + "&" + urllib.parse.quote_plus("/").replace("+", "%20").replace("*", "%2A").replace("%7E", "~") + "&" + stringToSign
 
-#This is the SignatureVersion
-SignatureVersion = {"SignatureVersion" : "1.0"}
+    #From here we start to sign with accessSecret
+    key = (accessSecret + '&').encode("UTF-8")
 
-#This is the Timestamp with type: yyyy-MM-dd'T'HH:mm:ss'Z'
-Timestamp = {"Timestamp" : datetime.datetime.now(tz = timezone('GMT')).strftime("%Y-%m-%dT%H:%M:%SZ")}
+    message = stringToSign.encode("UTF-8")
 
-#This is the Format
-Format = {"Format" : "XML"}
+    digester = hmac.new(key, message, hashlib.sha1)
 
-#API parameter
+    signature = str(base64.b64encode(digester.digest()), "UTF-8")
 
-#This is Action, "SendSms"
-Action = {"Action" : "SendSms"}
+    ulrsignature = urllib.parse.quote_plus(signature).replace("%7E", "~")
 
-#This is Version
-Version = {"Version" : "2017-05-25"}
+    finalrequest = "Signature=" + ulrsignature + "&" + parameterStr
 
-#This is RegionId
-RegionId = {"RegionId" : "cn-hangzhou"}
+    url = "http://dysmsapi.aliyuncs.com/?%s" % finalrequest
 
-#This is PhoneNumbers
-PhoneNumbers = {"PhoneNumbers" : "15300000001"}
-
-#This sign name SignName
-SignName = {"SignName" :  "季中策"}
-
-#This is template param
-TemplateParam = {"TemplateParam" : "{\"Code\":\"1234\"}"}
-
-#This is TemplateCode
-TemplateCode = {"TemplateCode" : "SMS_122299912"}
-
-#This is OutId
-OutId = {"OutId" : "123"}
-
-'''
-
-#This is the accesskey and accessSecret
-accessKeyId = "LTAIWAFaI2a8kv8n"
-
-accessSecret = "PX0FiQFOTauO0rt7WAtZR4l7s7FILw";
-
-QueryDict = {
-            "SignatureMethod" : "HMAC-SHA1",
-            "SignatureNonce" : str(uuid.uuid4()),
-            "AccessKeyId" : accessKeyId,
-            "SignatureVersion" : "1.0",
-            "Timestamp" : datetime.datetime.now(tz = timezone('GMT')).strftime("%Y-%m-%dT%H:%M:%SZ"),
-            "Format" : "XML",
-            "Action" : "SendSms",
-            "Version" : "2017-05-25",
-            "RegionId" : "cn-hangzhou",
-            "PhoneNumbers" : "15825679139",
-            "SignName" :  "季中策",
-            "TemplateParam" : "{\"Code\":\"1234\"}",
-            "TemplateCode" : "SMS_122299912",
-            "OutId" : "123"
-            }
-
-SortedQueryDict = dict()
-
-for key in sorted(QueryDict.keys()):
-    SortedQueryDict[key] = QueryDict[key]
-
-
-if "Signature" in SortedQueryDict.keys():
-    del SortedQueryDict["Signature"]
-
-
-#Here we will parse the query into url
-parameterStr = urllib.parse.urlencode(SortedQueryDict)
-
-#Then replace "+" with "%20"
-parameterStr.replace("+", "%20")
-
-#Then replace "+" with "%20"
-parameterStr.replace("*", "%2A")
-
-#Then replace "+" with "%20"
-parameterStr.replace("%7E", "~")
-
-#Then add POP with HTTPMethod + “&” + specialUrlEncode(“/”) + ”&” + specialUrlEncode(sortedQueryString)
-stringToSign = urllib.parse.quote_plus(parameterStr).replace("+", "%20").replace("*", "%2A").replace("%7E", "~")
-
-stringToSign = "GET" + "&" + urllib.parse.quote_plus("/").replace("+", "%20").replace("*", "%2A").replace("%7E", "~") + "&" + stringToSign
-
-#From here we start to sign with accessSecret
-key = bytes(accessSecret + "&", "utf-8")
-
-message = bytes(stringToSign, "utf-8")
-
-digester = hmac.new(key, message, hashlib.sha1)
-
-signature = str(base64.urlsafe_b64encode(digester.digest()), 'UTF-8')
-
-ulrsignature = urllib.parse.quote_plus(signature).replace("+", "%20").replace("*", "%2A").replace("%7E", "~")
-
-finalrequest = "Signature=" + ulrsignature + "&" + parameterStr
+    RESPONSE = requests.get(url)
 
 
-url = "http://dysmsapi.aliyuncs.com/?%s" % finalrequest
+    # print(urllib.parse.quote("/"))
 
-RESPONSE = requests.get(url)
+    # print(ulrsignature)
 
-print(RESPONSE.text)
+    print(RESPONSE.text)
+    print(RESPONSE.status_code)
 
-# print(stringToSign)
+
+def Construct_Parameter(QueryDict):
+    SortedQueryDict = dict()
+
+    for key in sorted(QueryDict.keys()):
+        SortedQueryDict[key] = QueryDict[key]
+
+
+    if "Signature" in SortedQueryDict.keys():
+        del SortedQueryDict["Signature"]
+
+    #Here we will parse the query into url
+    parameterStr = urllib.parse.urlencode(SortedQueryDict)
+
+    #Then replace "+" with "%20"
+    parameterStr.replace("%7E", "~")
+
+    return(parameterStr)
+
+
+
+
+SendSMS("13857788887")
