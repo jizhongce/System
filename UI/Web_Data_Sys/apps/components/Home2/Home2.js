e2b9dd4c1863bc71f3a4a1bd0e5316bad564d17a
jizhongce <jizhongce123@gmail.com>
2018-07-03 16:55:12 +0800

    delete order confirmation page

--- UI/Web_Data_Sys/apps/components/Home2/Home2.js
+++ UI/Web_Data_Sys/apps/components/Home2/Home2.js
@@ -60,7 +60,7 @@ import NavigationBar from 'react-native-navbar';
 import Status_Bar from '../Status_Bar.js';
 
 
-class Confirmation_Page_Header extends React.Component {
+class User_Home_Header extends React.Component {
   render() {
     return (
       <View>
@@ -76,7 +76,7 @@ class Confirmation_Page_Header extends React.Component {
 
 
 
-          <Text style={{fontSize: 20, color: 'black', fontWeight:'bold'}}> 收 银 台 </Text>
+          <Text style={{fontSize: 20, color: 'black', fontWeight:'bold'}}> 登 录 </Text>
 
 
 
@@ -93,7 +93,7 @@ class Confirmation_Page_Header extends React.Component {
 export default class Home2 extends Component<{}> {
 
   static navigationOptions = {
-    header: <Confirmation_Page_Header />
+    header: <User_Home_Header />
 }
 
   constructor(props) {
@@ -112,142 +112,7 @@ export default class Home2 extends Component<{}> {
 
       <KeyboardAvoidingView keyboardVerticalOffset={60} behavior={'position'} >
 
-        {/* Header */}
-
-        <View style={{marginTop: '2%', paddingLeft: 10, height: '7%', backgroundColor: 'white', justifyContent: 'center', alignItems: 'center'}}>
-
-          <Text style={{fontSize: 18}}> 需 支 付 : <Text style={{color: 'red'}}>9999</Text> </Text>
-
-        </View>
-
-        {/* Header */}
-
-
-        {/* Payment Method */}
-
-        <View style={{height: '80%'}}>
-
-
-          {/* online payment method  */}
-
-          <View style={{marginTop:10, backgroundColor: 'white'}}>
-
-            <View style={{borderBottomColor: 'black', borderBottomWidth: 1, paddingTop: 10, paddingBottom: 10}}>
-              <Text style={{fontSize: 16}}> 线 上 支 付 </Text>
-            </View>
-
-
-
-            <View style={{marginLeft: 5, paddingTop: 10, paddingBottom: 10, borderBottomColor: 'black', borderBottomWidth: 1, flexDirection: 'row',  alignItems: 'center'}}>
-
-
-              <View style={{width: '80%'}}>
-                <Text style={{fontSize: 17}}> 支 付 宝 支 付 </Text>
-
-              </View>
-
-              <View style={{width: '20%'}}>
-                <Icon name='radio-button-checked' />
-
-              </View>
-
-
-            </View>
-
-
-            <View style={{marginLeft: 5, paddingTop: 10, paddingBottom: 10, borderBottomColor: 'black', borderBottomWidth: 1, flexDirection: 'row',  alignItems: 'center'}}>
-
-
-                <View style={{width: '80%'}}>
-                  <Text style={{fontSize: 17}}> 微 信 支 付 </Text>
-
-                </View>
-
-                <View style={{width: '20%'}}>
-                  <Icon name='radio-button-unchecked' />
-
-                </View>
-
-
-            </View>
-
-
-            <View style={{marginLeft: 5, paddingTop: 10, paddingBottom: 10, borderBottomColor: 'black', borderBottomWidth: 1, flexDirection: 'row',  alignItems: 'center'}}>
-
-
-              <View style={{width: '80%'}}>
-                <Text style={{fontSize: 17}}> 银 行 卡 快 捷 支 付 </Text>
-
-              </View>
-
-              <View style={{width: '20%'}}>
-                <Icon name='radio-button-unchecked' />
-
-              </View>
-
-
-            </View>
-
-
-
-          </View>
-
-
-
-          {/* online payment method  */}
-
-
-
-          {/* offline payment method  */}
-
-          <View style={{marginTop:10, backgroundColor: 'white',}}>
-
-            <View style={{borderBottomColor: 'black', borderBottomWidth: 1, paddingTop: 10, paddingBottom: 10}}>
-              <Text style={{fontSize: 16}}> 线 下 支 付 </Text>
-            </View>
-
-
-            <View style={{marginLeft: 5, paddingTop: 10, paddingBottom: 10, borderBottomColor: 'black', borderBottomWidth: 1, flexDirection: 'row',  alignItems: 'center'}}>
-
-
-              <View style={{width: '80%'}}>
-                <Text style={{fontSize: 17}}> 银 行 转 账 </Text>
-
-              </View>
-
-              <View style={{width: '20%'}}>
-                <Icon name='radio-button-unchecked' />
-
-              </View>
-
-            </View>
-
-
-          </View>
-
-
-
-          {/* offline payment method  */}
-
-
-
-
-        </View>
-
-        {/* Payment Method */}
-
-
-        {/* Submit Payment */}
-
-        <View style={{height: '12%', width: '100%', backgroundColor: 'white', alignItems: 'center', justifyContent: 'center'}}>
-
-          <Text style={{fontSize: 17}}> 支 付 宝 支 付 : 9999 </Text>
-
-        </View>
-
-        {/* Submit Payment */}
-
-
+        
 
 
 
